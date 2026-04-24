cd47cd2cfe3ce955e2e7600031b86a168da8fcb1
dmitryplaymail-code <dmitryplaymail@gmail.com>
2026-04-24 20:20:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
 from db import init_db
 from handlers import router
@@ -13,7 +14,7 @@ logging.basicConfig(level=logging.INFO)
 async def main():
     init_db()
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dp = Dispatcher()
+    dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
 
     setup_scheduler(bot)
