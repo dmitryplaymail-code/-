103df6d8be73a0b9820ccf2f715ebedf3f5903a9
dmitryplaymail-code <dmitryplaymail@gmail.com>
2026-04-24 19:59:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
-BOT_TOKEN = os.getenv("8416719223:AAEgSgqS0ssBKjpZoHaeTr7OUgkq83lqgjg")
+# Токен вашего бота (замените на переменную окружения, если будете деплоить)
+BOT_TOKEN = "8416719223:AAEgSgqS0ssBKjpZoHaeTr7OUgkq83lqgjg"
+
 # Разрешённые пользователи (папа и мама) – замените на реальные ID
 ALLOWED_USERS = [6665577302, 502471632]
 
